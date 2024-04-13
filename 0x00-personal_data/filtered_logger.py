9cebda465a8fbf6c50f7881fa193eab20d301fbb
Golhun <geraldduah1@gmail.com>
2024-04-13 22:24:37 +0000

    update

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Redacting Formatter
+"""
+import re
+
+
+def filter_datum(fields: list[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """ returns the log message obfuscated """
+    return re.sub(r"(\w+)=([a-zA-Z0-9@\.\-\(\)\ \:\^\<\>\~\$\%\@\?\!\/]*)",
+                  lambda match: match.group(1) + "=" + redaction
+                  if match.group(1) in fields else match.group(0), message)
