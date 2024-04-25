96f263af0597b1200ad9f4a203d4ea6bfb01ee4a
Ernest-Geek <ernestampene1@gmail.com>
2024-04-25 22:05:59 +0000

    implement a logout function to respond to the DELETE /sessions route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -58,7 +58,7 @@ def logout() -> str:
     user = AUTH.get_user_from_session_id(session_id)
     if user is None:
         abort(403)
-    AUTH.destroy_session(user_id)
+    AUTH.destroy_session(user.id)
     return redirect("/")
 
 
