823dd9dd5587e55512fc591820eed5a6fafdecd9
Ernest-Geek <ernestampene1@gmail.com>
2024-04-25 01:33:45 +0000

    implement the Auth.register_user in the Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -15,3 +15,19 @@ def _hash_password(password: str) -> bytes:
     """Returns a bycrpt hash password
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Adds a new user to the database
+        """
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        raise ValueError("User {} already exists".format(email))
